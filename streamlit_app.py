6fa86bc2ff6388300ff91dce226816c0bbe3a540
rupeshpagariya <118935688+rupeshpagariya@users.noreply.github.com>
2022-11-23 16:09:32 -0500

    Update streamlit_app.py
    Added texts related to print

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit
 streamlit.title('Hey there! from Rupesh')
+
+streamlit.header('Learning Snowflake')
+
+streamlit.text('Metadata layer')
+streamlit.text('Virtual warehouse')
+streamlit.text('Storage')
+
+
+
+
