44f26cd19476913a50aef10649049185b7f7cbdb
rupeshpagariya <118935688+rupeshpagariya@users.noreply.github.com>
2022-11-23 16:35:03 -0500

    Update streamlit_app.py
    Added breakfast details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
-streamlit.title('Hey there! from Rupesh')
+streamlit.title('Tital changed! from Rupesh')
 
 streamlit.header('Learning Snowflake')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.text('Metadata layer')
 streamlit.text('Virtual warehouse')
