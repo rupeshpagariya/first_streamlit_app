b937c615c9dc680eaa216b015cf193233edc7f1c
rupeshpagariya <118935688+rupeshpagariya@users.noreply.github.com>
2022-11-24 14:28:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('Tital changed! from Rupesh')
 
 streamlit.header('Learning Snowflake')
-streamlit.header('\N{selfie}  Build Your Own Fruit Smoothie')
+streamlit.header('\N{accessibility}  Build Your Own Fruit Smoothie')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
