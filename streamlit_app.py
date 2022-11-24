1910ac32bff0c879db27bc66b90c6098d51128e4
rupeshpagariya <118935688+rupeshpagariya@users.noreply.github.com>
2022-11-24 14:32:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
+import pandas
 streamlit.title('Tital changed! from Rupesh')
 
 streamlit.header('Learning Snowflake')
-streamlit.header('\N{accessibility}  Build Your Own Fruit Smoothie')
+
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
@@ -12,5 +13,7 @@ streamlit.text('Virtual warehouse')
 streamlit.text('Storage')
 
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
